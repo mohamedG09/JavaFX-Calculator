aa1cc29ac4009b4e3673c1a18a5599a9c2e5d5dd
Mohamed <mohamed.abdelhady9999@gmail.com>
2019-01-25 11:26:49 +0400

    Buttons 0-9 Programmed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,66 @@
 package sample;
 
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Button butt0;
+    public Button butt1;
+    public Button butt2;
+    public Button butt3;
+    public Button butt4;
+    public Button butt5;
+    public Button butt6;
+    public Button butt7;
+    public Button butt8;
+    public Button butt9;
+    public Label mainlabel;
+    public Label historylabel;
+
+    public void insertNum(String s){
+        mainlabel.setText(mainlabel.getText()+s);
+    }
+
+    public void actionButt0(){
+        if(mainlabel.getText().charAt(0)=='0'){return;}
+        insertNum("0");
+    }
+
+    public void actionButt1(){
+        insertNum("1");
+    }
+
+    public void actionButt2(){
+        insertNum("2");
+    }
+
+    public void actionButt3(){
+        insertNum("3");
+    }
+
+    public void actionButt4(){
+        insertNum("4");
+    }
+
+    public void actionButt5(){
+        insertNum("5");
+    }
+
+    public void actionButt6(){
+        insertNum("6");
+    }
+
+    public void actionButt7(){
+        insertNum("7");
+    }
+
+    public void actionButt8(){
+        insertNum("8");
+    }
+
+    public void actionButt9(){
+        insertNum("9");
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello JAVA_FX");
-
+        primaryStage.setTitle("AbdelHady's Calculator");
         primaryStage.setScene(new Scene(root, 414, 749));
         primaryStage.show();
     }
